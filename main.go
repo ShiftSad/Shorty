c974edb78f03e53be1aecdbe39705f2d5adaf263
Sadness <rosalimavictor@gmail.com>
2025-02-06 15:47:18 -0300

    Gorm to save urls

--- main.go
+++ main.go
@@ -2,9 +2,31 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type Url struct {
+	gorm.Model
+	Short string `json:"short"`
+	Long  string `json:"long"`
+}
+
+var db *gorm.DB
+
 func main() {
+	// Initialize the database
+	var err error
+	db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Url{})
+
+	// Initialize the router
 	r := gin.Default()
 
 	r.GET(":short", redirectURL)
@@ -14,9 +36,23 @@ func main() {
 }
 
 func redirectURL(c *gin.Context) {
+	short := c.Param("short")
 
+	var url Url
+	db.First(&url, "short = ?", short)
+
+	c.Redirect(301, url.Long)
 }
 
 func shortenURL(c *gin.Context) {
+	var url Url
+	err := c.BindJSON(&url)
 
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	db.Create(&url)
+	c.JSON(200, gin.H{"short": url.Short})
 }
