9d3d8b725d89887c6949fb444acc8c8c1cddcf06
Sadness <rosalimavictor@gmail.com>
2025-02-07 12:23:45 -0300

    JS for the button

---
+++ static/script.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const shortenButton = document.getElementById("shortenButton");
+    const shortenInput = document.getElementById("shortenInput");
+    const shortAnotherButton = document.getElementById("shortAnotherButton");
+
+    shortenButton.addEventListener("click", async function(event) {
+        event.preventDefault();
+
+        if (shortenButton.innerText === "Shorten") {
+            await shorten();
+        }
+
+        if (shortenButton.innerText === "Copy") {
+            copyToClipboard();
+        }
+    });
+
+    shortAnotherButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        shortenInput.value = "";
+        shortenButton.innerText = "Shorten";
+        shortAnotherButton.style.display = "none";
+    });
+
+    function copyToClipboard() {
+        const copyText = document.getElementById("shortenInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(copyText.value).then(_ => console.log("Copied to clipboard"));
+    }
+
+    async function shorten() {
+        const url = shortenInput.value.trim();
+        if (url === "") {
+            return;
+        }
+
+        // Regex to check if it is a valid url
+        const urlRegex = new RegExp(/^(http|https):\/\/[^ "]+$/);
+        if (!urlRegex.test(url)) {
+            shortenInput.value = "Invalid URL";
+            return;
+        }
+
+        try {
+            const response = await fetch("/shorten", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ url })
+            });
+
+            const data = await response.json();
+            console.log(data);
+            if (data.error) {
+                shortenInput.value = data.error;
+                return;
+            }
+
+            // get current website domain
+            shortenInput.value = "https://" + window.location.hostname + ":" + window.location.port + "/" + data["short"];
+            shortenButton.innerText = "Copy";
+
+            shortAnotherButton.style.display = "inline-block";
+        } catch (error) {
+            console.error(error);
+            shortenInput.value = "An error occurred";
+        }
+    }
+});
